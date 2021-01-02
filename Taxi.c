19e52028ca2941c39d7c82563fd66c957b08c0c6
Lorenzo <lorenzo@Argos.local>
2021-01-02 23:12:34 +0100

    Metodo attach taxi

--- Taxi.c
+++ Taxi.c
@@ -21,27 +21,75 @@
  *	Deve ricordarsi di: dove si trova per scegliere il percorso.
  */	
 int x, y;
-int msg_queue_of_cell_key, map_shm_id, taxi_sem_id;
+int random_coordinates[2];
+int tmpx, tmpy;
+int msg_queue_of_cell_key, msg_queue_of_cell, map_shm_id, taxi_sem_id;
+struct sembuf accesso;
+struct sembuf rilascio;    
 map *pointer_at_map;
 
+
+void random_cell() {
+	srand(getpid());
+    /* Possibile loop infinito, dipende dai controlli */
+    do {
+        random_coordinates[0] = rand() % ((SO_HEIGHT-1) - 0 + 1) + 0; /* x */
+        random_coordinates[1] = rand() % ((SO_WIDTH-1) - 0 + 1) + 0; /* y */
+    } while (pointer_at_map->mappa[random_coordinates[0]][random_coordinates[1]].cell_type == 0);
+    tmpx = random_coordinates[0];
+    tmpy = random_coordinates[1];
+    /* Impostazione di accesso */
+    accesso.sem_num = pointer_at_map->mappa[tmpx][tmpy].reference_sem_number; 
+	accesso.sem_op = -1;
+	accesso.sem_flg = IPC_NOWAIT;
+	/* Impostazione di rilascio */
+	rilascio.sem_num = pointer_at_map->mappa[tmpx][tmpy].reference_sem_number;
+	rilascio.sem_op = +1;
+	rilascio.sem_flg = 0;
+}
 
 /********** ATTACH ALLA CELLA **********/
 /*
  *	Con questo metodo il processo taxi si attacca a una cella in modo casuale che trova
  *	tra le libere e quelle che non eccedono già la capacità.
  */
-#if 0
 void attach(map *pointer_at_map) {
-    int x,y;
-    srand(getpid());
-    do {
-        x = rand() % ((SO_HEIGHT-1) - 0 + 1) + 0; 
-        y = rand() % ((SO_WIDTH-1) - 0 + 1) + 0;
-    } while (pointer_at_map->mappa[x][y].cell_type == 0 && pointer_at_map->mappa[x][y].active_taxis < SO_CAP_MAX);
-} 
+#ifdef MAPPA_VALORI_CASUALI
 
+    random_cell();
+
+    /* Entro in sezione critica */
+    while (semop(taxi_sem_id, &accesso, 1) == -1) { /* Possibile loop infinito. Dipende dai controlli. */
+    	random_cell();
+    }
+    /* Ci parcheggiamo */
+    if (pointer_at_map->mappa[tmpx][tmpy].active_taxis <= pointer_at_map->mappa[tmpx][tmpy].taxi_capacity) {
+    	x = tmpx;
+    	y = tmpy;
+    }
+    semop(taxi_sem_id, &rilascio, 1);
+    
+    /* Verifico se la cella è 1 o 3 e prendo l'id della coda di messaggi. */
+    if (pointer_at_map->mappa[x][y].cell_type == 1 || pointer_at_map->mappa[x][y].cell_type == 3) {
+		msg_queue_of_cell_key = pointer_at_map->mappa[x][y].message_queue_key;
+	}
+	/* Ci attacchiamo alla coda di messaggi */
+	msg_queue_of_cell = msgget(msg_queue_of_cell_key, 0);
+	if (msg_queue_of_cell == -1){
+		perror("Sono un processo Taxi: non riesco ad attaccarmi alla coda di messaggi della mia cella.");
+	}
 #endif
-
+#ifndef MAPPA_VALORI_CASUALI	
+		
+	x = 2;
+	y = 2;
+	msg_queue_of_cell_key = pointer_at_map->mappa[x][y].message_queue_key;
+	msg_queue_of_cell = msgget(msg_queue_of_cell_key, 0);
+	if (msg_queue_of_cell == -1){
+		perror("Sono un processo Taxi: non riesco ad attaccarmi alla coda di messaggi della mia cella.");
+	}
+#endif
+}  
 /********** MAIN **********/
 /*
  *	All'interno della funzione main il processo deve prendere controllo del segmento di memoria
@@ -68,7 +116,7 @@ int main(int argc, char *argv[])
     if (taxi_sem_id == -1){
         perror("Processo Taxi: non riesco ad accedere al mio semaforo. Termino.");
         TEST_ERROR 
-            exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     /* Chiamo il metodo attach */
     /*attach(pointer_at_map);*/
